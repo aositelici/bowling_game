ad1c88f0626e73bbe6efc2b141d88a461ce562dc
liya <liya013@live.com>
2015-09-18 23:12:54 +0800

    refactor this class

--- src/model/frame.js
+++ src/model/frame.js
@@ -3,57 +3,83 @@
 function Frame() {
 }
 
-Frame.prototype.judgeType = function(string) {
-  if(string === 'X') {
-    this.score = 10;
-    this.count = 1;
-    this.nextCount = 2;
+Frame.prototype.judgeType = function (string) {
+  if (string === 'X') {
+    this.determineStrike();
   }
-  else if(string.indexOf('/')!== -1) {
-    var allScore = [];
-    var scoreArray = string.split('');
-    allScore.push(Number(scoreArray[0]));
-    allScore.push(10 - Number(scoreArray[0]));
-    this.score =allScore;
-    this.count = 2;
-    this.nextCount = 1;
+  else if (string.indexOf('/') !== -1) {
+    this.determineSpare(string);
   }
   else {
-    var allScore = [];
-    var scoreArray = string.split('');
-    scoreArray.forEach(function (score) {
-      allScore.push(isNaN(Number(score)) ? 0 : Number(score));
-    });
-    this.score =allScore;
-    this.count = 2;
-    this.nextCount = 0;
+    this.determineCommon(string);
   }
 };
 
-Frame.prototype.getScore = function(stringArray) {
+Frame.prototype.determineStrike = function() {
+  this.score = 10;
+  this.count = 1;
+  this.nextCount = 2;
+  this.type = 'STRIKE';
+};
+
+Frame.prototype.determineSpare = function(string) {
+  var allScore = [];
+  var scoreArray = string.split('');
+
+  scoreArray.forEach(function (score) {
+    allScore.push(isNaN(Number(score)) ? 10 - allScore[0] : Number(score));
+  });
+  this.score = allScore;
+  this.count = 2;
+  this.nextCount = 1;
+  this.type = 'SPARE';
+};
+
+Frame.prototype.determineCommon = function(string) {
+  var allScore = [];
+  var scoreArray = string.split('');
+
+  scoreArray.forEach(function (score) {
+    allScore.push(isNaN(Number(score)) ? 0 : Number(score));
+  });
+  this.score = allScore;
+  this.count = 2;
+  this.nextCount = 0;
+  this.type = 'COMMON';
+};
+
+Frame.prototype.getScore = function (stringArray) {
   this.judgeType(stringArray[0]);
   var score = 0;
 
-  if(this.score instanceof Array) {
-    score += this.score.reduce(function(a,b) {
+  if (this.type === 'STRIKE') {
+    score += this.score;
+  }
+  else {
+    score += this.score.reduce(function (a, b) {
       return a + b;
     })
   }
-  else {
-    score += this.score;
-  }
+  var nextStringArray = stringArray.slice(1);
+  score += this.getNextScore(nextStringArray);
+  return score;
+};
+
+Frame.prototype.getNextScore = function (array) {
   var i = 0;
-  while( i < this.nextCount) {
+  var score = 0;
+  while (i < this.nextCount) {
     var frame = new Frame();
-    frame.judgeType(stringArray[i+1]);
-    if(frame.score instanceof Array) {
-      for(var k = 0; k < frame.score.length && i < this.nextCount; k++,i++) {
-        score += frame.score[k];
-      }
-    }
-    else {
+    frame.judgeType(array[i]);
+
+    if (frame.type === 'STRIKE') {
       score += frame.score;
       i += frame.count;
+    }
+    else {
+      for (var k = 0; k < frame.score.length && i < this.nextCount; k++, i++) {
+        score += frame.score[k];
+      }
     }
   }
   return score;
